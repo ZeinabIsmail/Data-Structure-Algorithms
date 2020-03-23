46778f894368b7783a159f7ac0922481ed99f87f
ZeinabIsmail <55862902+ZeinabIsmail@users.noreply.github.com>
2020-03-23 03:32:29 +0200

    Add files via upload

---
+++ DynamicArrays.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float *arr;
+    int arrSize;
+    int fact;
+    float sum=0;
+    cin>>arrSize;
+    arr = new float [arrSize];
+    for(int i=0; i<arrSize; i++)
+    {
+        if(i==0)
+        {
+            fact=1;
+        }
+        else
+        {
+            for(int j=i; j>0; j--)
+            {
+                fact=i*j;
+            }
+        }
+        arr[i]=1.0/fact;
+    }
+
+    for(int i=0; i<arrSize; i++)
+    {
+        sum+=arr[i];
+
+    }
+    cout<<sum<<endl;
+    return 0;
+}
